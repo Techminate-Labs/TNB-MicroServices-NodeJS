4301fa3b4768e52db9e4e820ae5cec641a19dba6
SazidAhmed <sazidahmed.official@gmail.com>
2022-03-27 12:53:40 +0600

    done some clean up

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,20 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-//Middleware
-const { protect } = require('../app/middleware/AuthMiddleware')
-
 //Controllers
-const { register, login, authUser } = require('../app/Controllers/Auth/AuthController')
 const { getTransactions } = require('../app/Controllers/Account/Transaction')
 const { 
     createNewAccount, generatePublicKey, generateSignature, verifySignature,
 } = require('../app/Controllers/Account/Account')
 
-//Auth apis
-router.post('/register', register)
-router.post('/login', login)
-router.get('/authUser', protect, authUser)
 
 //Blockchain apis
 router.get('/getTransactions', getTransactions)
