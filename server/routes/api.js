8b2a45e76363f55bda50e2f5122d41142276b212
SazidAhmed <sazidahmed.official@gmail.com>
2022-03-30 23:34:04 +0600

    login & register api added

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,15 +2,23 @@ const express = require('express')
 const router = express.Router()
 
 //Controllers
+const { register, login, authUser } = require('../app/Controllers/Auth/AuthController')
+
 const { getTransactions } = require('../app/Controllers/Account/Transaction')
 const { 
     createNewAccount, generatePublicKey, generateSignature, verifySignature,
     signMessage, createBlockMessage, sendCoins
 } = require('../app/Controllers/Blockchain/Account')
 
+//Auth apis
+router.post('/register', register)
+router.post('/login', login)
+router.get('/authUser', protect, authUser)
+
+//Accounts
+
 
 //Blockchain apis
-//Accounts
 router.get('/createNewAccount', createNewAccount)
 router.get('/generatePublicKey/', generatePublicKey)
 router.get('/generateSignature/', generateSignature)
